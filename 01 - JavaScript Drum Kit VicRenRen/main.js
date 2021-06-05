411f1cda37777bce1441d432c6110dc8f025d611
VicRenRen <kiiroinihil@gmail.com>
2021-06-05 01:00:11 +0100

    1. we created an event listiner that listens for keydown event that calls a function

--- 01 - JavaScript Drum Kit VicRenRen/main.js
+++ 01 - JavaScript Drum Kit VicRenRen/main.js
@@ -1,1 +1,13 @@
-console.log("Victor is working like crazy!");
+// console.log("Victor's Drum Kit JS30 challenge!");
+// We are making a javascript drumkit that responds to keyboard keys when pressed by a human.
+// By 'responds' we mean produce sound, just like real drums!
+// keycode.info gives a number associated with key pressed, works with most keys and with most languages but it aint perfect
+// 1.Firstly we need to listen for keydown event
+// What is window?
+// The window object represents the browser's window.
+// The window object represents an open window in a browser.
+// funtion(e) -> `e` stands for an event
+//
+window.addEventListener("keydown", function (e) {
+  console.log(e);
+});
