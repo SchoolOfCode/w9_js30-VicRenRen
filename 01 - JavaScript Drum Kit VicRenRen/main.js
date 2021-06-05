37866ebd95aff1ad8fec777c800f8323c3f941a5
VicRenRen <kiiroinihil@gmail.com>
2021-06-05 03:23:59 +0100

    function was removed from event listener to its own thing

--- 01 - JavaScript Drum Kit VicRenRen/main.js
+++ 01 - JavaScript Drum Kit VicRenRen/main.js
@@ -25,14 +25,16 @@
 // Whats happening now? We want the animation fhen finished to reverse back
 // we created a function that removes prievously added playing that in css was doing the animation
 
-window.addEventListener("keydown", function (e) {
+window.addEventListener("keydown");
+
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; // stops the entire function
   audio.currentTime = 0; // rewind to the start
   audio.play();
   key.classList.add("playing");
-});
+}
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return; // skip if not transormed
